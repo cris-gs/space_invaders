c8d732a7e415eb4de068c39bf92ef2af5d46dcb9
cris-gs <cristophergs2001@gmail.com>
2026-05-03 05:32:23 -0600

    Change entity icons

--- src/entities/bullet.py
+++ src/entities/bullet.py
@@ -1,5 +1,5 @@
 class Bullet:
-    def __init__(self, x: int, y: int, direction: int = -1, symbol: str = "|"):
+    def __init__(self, x: int, y: int, direction: int = -1, symbol: str = "│"):
         self.x = x
         self.y = y
         self.direction = direction
--- src/entities/player.py
+++ src/entities/player.py
@@ -1,5 +1,5 @@
 class Player:
-    def __init__(self, x: int, y: int, symbol: str = "A"):
+    def __init__(self, x: int, y: int, symbol: str = "▲"):
         self.x = x
         self.y = y
         self.symbol = symbol
